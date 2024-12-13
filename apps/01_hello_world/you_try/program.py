1f4a0c6152dc303a0cb6cbdbf5a811fd583cdf31
NicholasNgaruiya <nicholasngaruiya387@gmail.com>
2024-12-13 05:07:34 +0300

    (NEW) Created the Hello App

---
+++ apps/01_hello_world/you_try/program.py
@@ -0,0 +1,12 @@
+print('---------------------------------')
+print('           HELLO WORLD')
+print('---------------------------------')
+print()
+
+# TODO: To do something
+
+user_text = input('What is your name? ')
+# greeting= f'Nice to meet you {user_text}'
+greeting = 'Nice to meet you ' + user_text
+
+print(greeting)
